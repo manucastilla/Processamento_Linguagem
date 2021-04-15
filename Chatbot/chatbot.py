e27dd35d99890accbb143b2190d2379e7e212f5c
manucastilla <manuela.castilla@gmail.com>
2021-04-15 15:08:39 -0300

    feito o multinomial

--- Chatbot/chatbot.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("Pasta1.xlsx")
-
-print(df)
---
+++ Chatbot/chatbot_training.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from nltk import word_tokenize
+from tqdm import tqdm
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+
+
+def limpa_tudo(df):
+    # removendo minusculas
+    df = df.lower()
+    # remove todos os símbolos exceto $
+    df = re.sub(r"\W", " ", df)
+    # tirar o foxbot
+    df = df.replace("foxbot", "")
+    return df
+
+
+def main():
+
+    # lendo o excel
+    df = pd.read_excel("sentencas.xlsx")
+
+    # limpando o dataset
+    df["Sentença"] = df["Sentença"].apply(limpa_tudo)
+
+    # separar em dataset de treinamento e teste
+    X_train, X_test, y_train, y_test = train_test_split(
+        df["Sentença"], df["Intenção"], test_size=0.2, random_state=42)
+
+    # classificador naive-Bayes
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(X_train)
+    model = MultinomialNB()
+    model.fit(X, y_train)
+
+    countsTest = vectorizer.transform(X_test)
+
+    y_pred = model.predict(countsTest)
+    acc = accuracy_score(y_test, y_pred)
+    return acc
+
+
+if __name__ == "__main__":
+    print(main())
